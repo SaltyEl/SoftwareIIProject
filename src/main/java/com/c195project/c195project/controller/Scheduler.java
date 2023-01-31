eb266a6910809a3456edfa24b512cbc591188509
Blake Ramsey <elliottramsey1988@gmail.com>
2023-01-31 09:18:39 -0500

    Scheduler Table Info Added

--- src/main/java/com/c195project/c195project/controller/Scheduler.java
+++ src/main/java/com/c195project/c195project/controller/Scheduler.java
@@ -1,11 +1,13 @@
 package com.c195project.c195project.controller;
 
+import helper.AppointmentDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Scheduler implements Initializable {
@@ -16,7 +18,11 @@ public class Scheduler implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        try {
+            appointmentTableView.setItems(AppointmentDAO.getAppointmentList());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void onAppointmentAddButtonClick(ActionEvent actionEvent) {
--- src/main/java/helper/AppointmentDAO.java
+++ src/main/java/helper/AppointmentDAO.java
@@ -2,6 +2,7 @@ package helper;
 
 import com.c195project.c195project.model.Appointment;
 import com.c195project.c195project.model.Customer;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
@@ -20,7 +21,27 @@ public class AppointmentDAO {
         return numOfAppts;
     }
 
-    public static ObservableList<Appointment> getAppointmentList(){
-
+    public static ObservableList<Appointment> getAppointmentList() throws SQLException {
+        String queryStmt = "SELECT * FROM client_schedule.appointments";
+        JDBC.openConnection();
+        Query.querySQL(queryStmt);
+        ResultSet result = Query.getResult();
+        ObservableList<Appointment> resultList = FXCollections.observableArrayList();
+        while(result.next()){
+            Appointment nextAppointment = new Appointment();
+            nextAppointment.setId(result.getInt("Appointment_ID"));
+            nextAppointment.setTitle(result.getString("Title"));
+            nextAppointment.setDescription(result.getString("Description"));
+            nextAppointment.setLocation(result.getString("Location"));
+            nextAppointment.setType(result.getString("Type"));
+            nextAppointment.setStartDateTime(result.getString("Start"));
+            nextAppointment.setEndDateTime(result.getString("End"));
+            nextAppointment.setContactID(result.getInt("Contact_ID"));
+            nextAppointment.setUserID(result.getInt("User_ID"));
+            nextAppointment.setCustomerID(result.getInt("Customer_ID"));
+            resultList.add(nextAppointment);
+        }
+        JDBC.closeConnection();
+        return resultList;
     }
 }
